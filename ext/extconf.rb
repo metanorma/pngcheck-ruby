58bec67c811b09184245317e3ccf41eb3284561f
Maxim [maxirmx] Samsonov <m.samsonov@computer.org>
2022-09-25 23:35:43 +0300

    Fixed recipe issues

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 $LOAD_PATH << File.expand_path(File.join(File.dirname(__FILE__), "../lib"))
-PNGCHECK_ARCH = "pngcheck-3.0.3.tar.gz"
 require "pngcheck/recipe"
 
 recipe = PngCheck::Recipe.new
