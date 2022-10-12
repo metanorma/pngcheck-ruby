04ed33a35bb9645015b14010dc239cdf97082ea0
Alexande B <abobrikovich@gmail.com>
2022-10-12 09:28:39 +0200

    metanorma/pngcheck#4 add release-tag workflow

--- ext/layout.rb
+++ ext/layout.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-PNGCHECK_VER = "3.0.4"
-PNGCHECK_ARCH = "pngcheck-#{PNGCHECK_VER}.tar.gz"
-PNGCHECK_URI = "https://github.com/metanorma/pngcheck/archive/refs/tags/metanorma-v#{PNGCHECK_VER}.tar.gz"
+PNGCHECK_ARCH = "pngcheck-#{PngCheck::VERSION}.tar.gz"
+PNGCHECK_URI = "https://github.com/metanorma/pngcheck/archive/refs/tags/metanorma-v#{PngCheck::VERSION}.tar.gz"
 PNGCHECK_LOCAL = "ext/#{PNGCHECK_ARCH}"
+PNGCHECK_SHA = "f8ff6033dc0008a90a468213a96ac5d72476b1e59378e267341fcf3734ebb2b3"
