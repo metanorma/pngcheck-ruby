17c979b3f5f3098d6c211f076e79a50b8093fe83
Alexande B <abobrikovich@gmail.com>
2022-11-01 08:55:43 +0100

    Use right version in pacjage URI

--- ext/layout.rb
+++ ext/layout.rb
@@ -2,6 +2,6 @@
 
 PNGCHECK_VERSION = PngCheck::VERSION
 PNGCHECK_ARCH = "pngcheck-#{PNGCHECK_VERSION}.tar.gz"
-PNGCHECK_URI = "https://github.com/metanorma/pngcheck/archive/refs/tags/metanorma-v#{PngCheck::VERSION}.tar.gz"
+PNGCHECK_URI = "https://github.com/metanorma/pngcheck/archive/refs/tags/metanorma-v#{PNGCHECK_VERSION}.tar.gz"
 PNGCHECK_LOCAL = "ext/#{PNGCHECK_ARCH}"
 PNGCHECK_SHA = "f8ff6033dc0008a90a468213a96ac5d72476b1e59378e267341fcf3734ebb2b3"
